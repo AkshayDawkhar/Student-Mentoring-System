45ab820e49b6b2a1819cf7e05a5aff29dea8fded
AkshayDawkhar <akshaydawkhar59@gmail.com>
2022-08-29 00:51:20 -0400

    first commit

---
+++ formfild/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FormfildConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'formfild'
---
+++ formfild/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def formfild(request):
+    return render(request,'formfild.html')
